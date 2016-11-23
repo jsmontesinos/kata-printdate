4c3fcfad976ec5e6517743d3e166d6044961128b
Juan Sánchez <jsmontesinos@gmail.com>
2016-11-23 20:56:49 +0100

    Begin testing in console

--- src/test/java/com/jsmontesinos/kataprintdate/PrintDateTest.java
+++ src/test/java/com/jsmontesinos/kataprintdate/PrintDateTest.java
@@ -5,8 +5,11 @@ import org.junit.Test;
 public class PrintDateTest {
 	
 	@Test
-	public void should_print_today_date(){
+	public void should_print_current_date(){
+		PrintDate printDate = new PrintDate();
 		
+		printDate.printCurrentDate();
 		
+		// How to assert printed date is correct?
 	}
 }
