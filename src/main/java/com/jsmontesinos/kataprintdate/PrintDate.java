a38e968cbc7d21c8fe0af53ef321dcd836b79ba5
Juan Sánchez <jsmontesinos@gmail.com>
2016-11-23 21:01:04 +0100

    Print Date refactor -> Extract method

--- src/main/java/com/jsmontesinos/kataprintdate/PrintDate.java
+++ src/main/java/com/jsmontesinos/kataprintdate/PrintDate.java
@@ -5,7 +5,11 @@ import java.util.Date;
 public class PrintDate {
 	
 	public void printCurrentDate() { 
-		System.out.println(new Date()); 
+		System.out.println(getCurrentDate()); 
+	}
+
+	private Date getCurrentDate() {
+		return new Date();
 	}
 
 }
